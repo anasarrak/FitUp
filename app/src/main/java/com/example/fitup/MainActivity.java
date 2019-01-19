28363ebe02f439fad4c7b925fa73ec9fcf3fb54b
Unknown <anas.mchichou@ikasle.egibide.org>
2019-01-19 12:09:46 +0100

    Fix unsuccessful connection

--- app/src/main/java/com/example/fitup/MainActivity.java
+++ app/src/main/java/com/example/fitup/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.fitup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseFirestore nFirestore;
+    private Button saveButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        nFirestore = FirebaseFirestore.getInstance();
+        saveButton = (Button) findViewById(R.id.saveBtn);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                System.out.println("clickao");
+                Map<String,String> userMap = new HashMap<>();
+                userMap.put("steps","10");
+                userMap.put("top","10");
+                nFirestore.collection("user").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(MainActivity.this,"ADDED",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
