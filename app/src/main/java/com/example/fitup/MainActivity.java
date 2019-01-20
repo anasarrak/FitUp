f5b7ad1e80d49e200f946c237a314e9c657277bd
Unknown <anas.mchichou@ikasle.egibide.org>
2019-01-20 14:07:06 +0100

    Add current steps to firebase

--- app/src/main/java/com/example/fitup/MainActivity.java
+++ app/src/main/java/com/example/fitup/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.fitup;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Goal;
+import com.google.android.gms.fitness.data.Subscription;
 import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.request.GoalsReadRequest;
 import com.google.android.gms.fitness.result.DataReadResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,23 +40,35 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, SensorEventListener {
     private FirebaseFirestore nFirestore;
     int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
     private Button ajustes;
     static final String LOG_TAG = "FitUP!";
+    boolean running = false;
+    TextView steps;
+    SensorManager sensorManager;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
         nFirestore = FirebaseFirestore.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        login();
 
         FitnessOptions fitnessOptions = FitnessOptions.builder()
                 .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
@@ -58,23 +84,33 @@ public class MainActivity extends AppCompatActivity {
         } else {
             accessGoogleFit();
         }
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        buildFitnessClient();
 
-        /*saveButton = (Button) findViewById(R.id.saveBtn);
-        saveButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                System.out.println("clickao");
-                Map<String,String> userMap = new HashMap<>();
-                userMap.put("steps","10");
-                userMap.put("top","10");
-                nFirestore.collection("user").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+    }
+
+    private void login() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(MainActivity.this,"ADDED",Toast.LENGTH_LONG).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("User", "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            System.out.println("The user is : "+user.getUid());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("User", "signInAnonymously:failure", task.getException());
+                            /*Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();*/
+                        }
+
+                        // ...
                     }
                 });
-            }
-        });*/
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
@@ -83,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private void accessGoogleFit() {
         Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
@@ -120,5 +157,63 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(LOG_TAG, "onComplete()");
                     }
                 });
+    }
+
+    private void buildFitnessClient() {
+        // Create the Google API Client
+        GoogleApiClient mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addConnectionCallbacks(this)
+                .useDefaultAccount().build();
+        mClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //fetchUserGoogleFitData(selectedDate);
+
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor!= null){
+            sensorManager.registerListener(this,countSensor,sensorManager.SENSOR_DELAY_UI);
+        }else{
+            Toast.makeText(this,"SENSOR NOT FOUND",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        //sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running){
+            steps = findViewById(R.id.steps);
+            Map<String,String> userMap = new HashMap<>();
+            userMap.put("steps",String.valueOf(event.values[0]));
+            userMap.put("top","10");
+            nFirestore.collection("user").document(mAuth.getCurrentUser().getUid()).set(userMap);
+            steps.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
