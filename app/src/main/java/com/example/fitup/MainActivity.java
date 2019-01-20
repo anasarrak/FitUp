55355b8060dffe9d6d4e2e78a462338bd8b2da24
Unknown <anas.mchichou@ikasle.egibide.org>
2019-01-20 21:11:34 +0100

    update steps with Google Fit

--- app/src/main/java/com/example/fitup/MainActivity.java
+++ app/src/main/java/com/example/fitup/MainActivity.java
@@ -24,7 +24,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.data.Goal;
 import com.google.android.gms.fitness.data.Subscription;
 import com.google.android.gms.fitness.request.DataReadRequest;
@@ -51,15 +53,19 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
 
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, SensorEventListener {
     private FirebaseFirestore nFirestore;
     int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
+    private static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
     private Button ajustes;
     static final String LOG_TAG = "FitUP!";
     boolean running = false;
     TextView steps;
     SensorManager sensorManager;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private GoogleApiClient mClient = null;
+    private static final String TAGFit = "FitActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +74,26 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         nFirestore = FirebaseFirestore.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
 
+        setContentView(R.layout.activity_main);
+
+
+
+        FitnessOptions fitnessOptions =
+                FitnessOptions.builder()
+                        .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                        .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                        .build();
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this,
+                    REQUEST_OAUTH_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            subscribe();
+        }
+        
+/*
         login();
 
         FitnessOptions fitnessOptions = FitnessOptions.builder()
@@ -83,11 +109,31 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     fitnessOptions);
         } else {
             accessGoogleFit();
-        }
+        }*/
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        buildFitnessClient();
 
     }
+
+    public void subscribe() {
+        // To create a subscription, invoke the Recording API. As soon as the subscription is
+        // active, fitness data will start recording.
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .addOnCompleteListener(
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.i(TAGFit, "Successfully subscribed!");
+                                } else {
+                                    Log.w(TAGFit, "There was a problem subscribing.", task.getException());
+                                }
+                            }
+                        });
+        }
+
+
 
     private void login() {
         mAuth.signInAnonymously()
@@ -102,72 +148,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("User", "signInAnonymously:failure", task.getException());
-                            /*Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();*/
                         }
-
-                        // ...
                     }
                 });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit();
-            }
-        }
-    }
-
-    private void accessGoogleFit() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.YEAR, -1);
-        long startTime = cal.getTimeInMillis();
 
 
-        DataReadRequest readRequest = new DataReadRequest.Builder()
-                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                //El bucket siguiente es necesario, sino me casca con : java.lang.IllegalStateException: Must specify a valid bucketing strategy while requesting aggregation
-                .bucketByTime(1, TimeUnit.DAYS)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-                .build();
-
-
-
-        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
-                .readData(readRequest)
-                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
-                    @Override
-                    public void onSuccess(DataReadResponse dataReadResponse) {
-                        Log.d(LOG_TAG, "onSuccess()");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(LOG_TAG, "onFailure()", e);
-                    }
-                })
-                .addOnCompleteListener(new OnCompleteListener<DataReadResponse>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DataReadResponse> task) {
-                        Log.d(LOG_TAG, "onComplete()");
-                    }
-                });
-    }
-
-    private void buildFitnessClient() {
-        // Create the Google API Client
-        GoogleApiClient mClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.HISTORY_API)
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
-                .addConnectionCallbacks(this)
-                .useDefaultAccount().build();
-        mClient.connect();
-    }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -184,8 +171,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onResume() {
         super.onResume();
+        //connectFitness();
         running = true;
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
         if (countSensor!= null){
             sensorManager.registerListener(this,countSensor,sensorManager.SENSOR_DELAY_UI);
         }else{
@@ -193,22 +182,46 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         }
     }
 
-    @Override
+        @Override
     protected void onPause() {
         super.onPause();
         running = false;
         //sensorManager.unregisterListener(this);
     }
-
+    //Calculate the current steps using the sensor and add it to Firebase
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (running){
             steps = findViewById(R.id.steps);
-            Map<String,String> userMap = new HashMap<>();
-            userMap.put("steps",String.valueOf(event.values[0]));
-            userMap.put("top","10");
-            nFirestore.collection("user").document(mAuth.getCurrentUser().getUid()).set(userMap);
-            steps.setText(String.valueOf(event.values[0]));
+            final Map<String,String> userMap = new HashMap<>();
+            //userMap.put("steps",String.valueOf(event.values[0]));
+            //userMap.put("top","10");
+
+            Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                    .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<DataSet>() {
+                                @Override
+                                public void onSuccess(DataSet dataSet) {
+                                    long total =
+                                            dataSet.isEmpty()
+                                                    ? 0
+                                                    : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                                    Toast.makeText(MainActivity.this, String.valueOf(total), Toast.LENGTH_SHORT).show();
+                                    userMap.put("steps",String.valueOf(total));
+                                    userMap.put("top","9");
+                                    nFirestore.collection("user").document(mAuth.getCurrentUser().getUid()).set(userMap);
+                                    steps.setText(String.valueOf(total));
+
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAGFit, "There was a problem getting the step count.", e);
+                                }
+                            });
         }
     }
 
